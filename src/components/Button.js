0c823cf4057fa5e2744646b70309cf3b8f4d3f3b
Ali Zamani <Ali.zmn@hotmail.com>
2020-12-28 13:18:40 -0500

    finished DayList, DayListItem and nav with setDay

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,21 @@ import React from "react";
 
 import "components/Button.scss";
 
+const classnames = require("classnames");
 export default function Button(props) {
-   return <></>;
+   let buttonClass = classnames("button", {" button--confirm": props.confirm, " button--danger": props.danger});
+   // if (props.confirm) {
+   //    buttonClass += " button--confirm";
+   // } else if (props.danger) {
+   //    buttonClass += " button--danger";
+   // }
+   return (
+     <button
+       disabled={props.disabled}
+       onClick={props.onClick}
+       className={buttonClass}
+     >
+       {props.children}
+     </button>
+   );
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+
+const classNames = require("classnames");
+
+export default function DayListItem(props) {
+  const itemClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": props.spots===0});
+  const formatSpots = (remainSpot) => {
+    switch(remainSpot) {
+      case 0:
+        return "no spots remaining";
+      case 1:
+        return "1 spot remaining";
+      default:
+        return `${remainSpot} spots remaining`;
+    }
+  };
+  return (
+    <li className={itemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
